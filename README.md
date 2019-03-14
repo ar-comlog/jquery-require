3459e3270e5dd713f1ec456d7f41e4967027bc81
ar <a.rau@comlog.org>
2019-03-14 12:11:23 +0100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# jquery-require
+# Simple require plugin for jQuery
+
+Compatible to node's require and requirejs. Supports require JSON files. Supports browser cache.
+Firefox and Chrome debugger support (# sourceURL).
+
+#### Example 1:
+##### /js/myfile.js:
+```javascript
+module.exports = function(arg) {
+	...
+}
+```
+##### HTML Script tag:
+```javascript
+$.require('/js/myfile.js', function(mod) {
+	mod('arguments');
+});
+```
+
+#### Example 2:
+##### /js/myfile.js:
+```javascript
+module.exports = function(arg) {
+	this.data = 'abc';
+	...
+}
+```
+##### HTML Script tag:
+```javascript
+$.require('/js/myfile.js', function(mod) {
+	var obj = new mod('arguments');
+});
+```
+
+#### Example 3:
+##### /js/myfile.js:
+```javascript
+// anly exports works to :-)
+exports = {
+	data: 'abc'
+}
+```
+##### HTML Script tag:
+```javascript
+$.require('/js/myfile.js', function(mod) {
+	console.info(mod.data); // output abc
+	mod.data = 'xyz';
+});
+
+$.require('/js/myfile.js', function(mod) {
+	console.info(mod.data); // output xyz
+});
+```
+
+#### Example 4:
+##### /js/myfile.js:
+```javascript
+// anly exports works to :-)
+var global_var = {data: 'abc'};
+```
+##### HTML Script tag:
+```javascript
+$.require({'/js/myfile.js', exports: 'global_var'}, function(mod) {
+	console.info(mod.data); // output abc
+});
+```
+
+#### Example 5:
+##### /js/myfile.js:
+```javascript
+// anly exports works to :-)
+var global_var = {data: 'abc'};
+```
+##### HTML Script tag:
+```javascript
+$.require({'/js/myfile.js', exports: 'global_var', cache: false, async: false}, function(mod) {
+	console.info(mod.data); // output abc
+});
+```
---
+++ jquery.require.js
@@ -0,0 +1,77 @@
+(function($) {
+	var modules = {};
+
+	/**
+	 * @param {Object|string} src kann mit Rückgabe-Variablennamen erweitert werden (z.B. exports: 'io')
+	 * @param {function} cb
+	 * @return {jqXHR}
+	 */
+	$.require = function (src, cb) {
+		if (typeof src == 'string') src = {url: src};
+
+		var link = document.createElement("a");
+		link.href = src.url;
+		src.url = link.href;
+
+		// modul wurde bereits abgefragt
+		if (modules[src.url] && typeof modules[src.url].module != "undefined") {
+			cb(modules[src.url].module.exports, modules[src.url]);
+			return modules[src.url];
+		}
+
+		// laden erfolgreich
+		var done = function (data, textStatus, jqXHR) {
+			if (src.exports) data += "\ntry { exports = "+src.exports+"; } catch(e) {}";
+			data += "\n//# sourceURL="+src.url;
+
+			modules[src.url].module = {exports: null};
+
+			(function() {
+				var module = { exports: null };
+				var exports = null;
+				try {
+					var res = eval(data);
+					if (!module.exports) module.exports = exports || res;
+					modules[src.url].module = module;
+					cb(modules[src.url].module.exports, data, textStatus, jqXHR);
+				} catch (errorThrown) {
+					modules[src.url].textStatus = 'error';
+					modules[src.url].error = errorThrown;
+					cb(null, modules[src.url]);
+				}
+			})();
+		};
+
+		// laden fehlgeschlagen
+		var fail = function (jqXHR, textStatus, errorThrown) {
+			modules[src.url].module = {exports: null};
+			modules[src.url].textStatus = 'error';
+			modules[src.url].error = errorThrown;
+			cb(null, modules[src.url]);
+		};
+
+		// modul wird geladen
+		if (modules[src.url]) {
+			modules[src.url].done(done);
+			return modules[src.url];
+		}
+
+		if (modules[src.url]) {
+			if (modules[src.url].module)
+				cb(modules[src.url].module.exports);
+			else
+				modules[src.url].done(done());
+		}
+
+		var opt = $.extend({
+			dataType: 'text',
+			cache: true
+		}, src);
+
+		modules[src.url] = $.ajax(opt).done(done).fail(fail);
+
+		return modules[src.url];
+	};
+
+	$.require.modules = modules;
+})(jQuery);
